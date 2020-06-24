8e9b1ff7f817140e696ccdd0b5bbe7b77406752f
Renzo Oldani <oldanirenzo@gmail.com>
2020-06-24 04:57:34 -0300

    Segundo commit

---
+++ docs/precache-manifest.9fdcefcc5383afc6165c8f2e2cd2a817.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0730aa8da448613cfbfc73096789d29b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f04c0b021f8687c12902",
+    "url": "/static/css/main.75f21ca7.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f04c0b021f8687c12902",
+    "url": "/static/js/main.33eb8aa6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
